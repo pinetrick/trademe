e2a55760445abc2b31ce9513499fa9bfae2a7fb9
Pine C320 <admin@intbr.com>
2021-09-06 18:22:47 +1200

    Resource import

--- app/src/main/java/com/pine/mytrademetest/MainListings.kt
+++ app/src/main/java/com/pine/mytrademetest/MainListings.kt
@@ -3,9 +3,9 @@ package com.pine.mytrademetest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainListings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_listing)
     }
 }
